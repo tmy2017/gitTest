04a26d3265a53dfb3a92093135bfafc29a096924
Tommy Kao <tommykao@Tmy-Work-MacBook-Air.local>
2018-06-22 09:47:03 +0200

    hihi strange why start with merge?

--- abc.c
+++ abc.c
@@ -3,25 +3,7 @@ hihi zz20171125_1816
 azefazef
 zz20171125_1756
 master remote
-<<<<<<< HEAD
-azefazefazioejriazott
-azefaaizefjaozeiff
-azer
-n()
-{
-	for(...)
-	{
-		for(...)
-		{
-			if(...)
-			{
-			}
-		}
-	}
-}
-=======
 azeifjaezf
 azefazf
 
 function
->>>>>>> 8b3ffb74525828a40de04907796922d99f2795d1
