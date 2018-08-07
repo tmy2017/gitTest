6b8927b38379dbe73c72e0b81d1310a39114c824
Tommy Kao <tommykao@Tmy-Work-MacBook-Air.local>
2018-08-07 06:57:01 +0200

    qdsf

--- yoyo.c
+++ yoyo.c
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-aiozefjoiazejf tmy1 top
-<<<<<<< HEAD
-ioazjeifoajzef
-=======
-zz20171124_0000
->>>>>>> tmy1
-=======
 zz20171125_1744
->>>>>>> 55867b78ad599a4891ddb9d1a4583ada01a8b698
 qsdfqsfazefiazef
 qsdfqsdf
-ioqsdjfoiqsdjfoijqsdfoij master at bottom
-master bottom again zz20171124_0014
+zz20180709_2325
+zz20180709_2330
